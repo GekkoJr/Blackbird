07369f3a1fa722a30ddbb5382e94aa418d297a01
GekkoJr <gekkojr@proton.me>
2023-09-28 14:31:16 +0200

    added login

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function loginPage()
+    {
+        return view('login');
+    }
+
+    public function createUser(Request $request)
+    {
+        $validated = $request->validate([
+            'email' => 'required|unique:user|email',
+            'username' => 'required|unique:user|alpha_num|between:3,50',
+            'password' => 'required',
+            'display_name' => 'required'
+        ]);
+
+        $user = new User;
+        $user->username = $validated['username'];
+        $user->password = Hash::make($validated['password']);
+        $user->email = $validated['email'];
+        $user->display_name = $validated['display_name'];
+        $user->save();
+
+    }
+
+    public function login(Request $request)
+    {
+        $validated = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if(Auth::attempt($validated)) {
+            return redirect(route('home'));
+        }
+
+        return back()->withErrors([
+            'email' => 'your email or password is incorecct'
+        ]);
+    }
+
+}

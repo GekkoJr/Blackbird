cce53c29c4902277739040a675b41a70595e123e
GekkoJr <gekkojr@proton.me>
2023-10-11 11:28:36 +0200

    app now compiles scss via vite

--- resources/views/components/layouts/welcome.blade.php
+++ resources/views/components/layouts/welcome.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        @vite('resources/css/welcome.css')
+        @vite('resources/css/welcome.scss')
         <title>{{ $title ?? 'Page Title' }}</title>
     </head>
     <body>

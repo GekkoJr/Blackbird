685e7e5c793af4d8f543ac77ba4f35d543d789df
GekkoJr <gekkojr@proton.me>
2023-10-12 20:27:47 +0200

    improved chat speed?

--- resources/views/livewire/app-components/send-message.blade.php
+++ resources/views/livewire/app-components/send-message.blade.php
@@ -1,6 +1,6 @@
 <div class="sendMessage">
     {{-- Be like water. --}}
-    <input type="text" wire:model="message" wire:keydown.enter="sendMessage">
+    <input type="text" wire:model.live="message" wire:keydown.enter="sendMessage">
     {{-- This is really slooooow, optimizing later, by swapping this to api based --}}
     <button  wire:click="sendMessage"><span class="material-symbols-outlined">send</span></button>
 </div>

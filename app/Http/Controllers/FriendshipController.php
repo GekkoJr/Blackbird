4b4721175b3a18fe4721e060265a686afd29b02a
GekkoJr <gekkojr@proton.me>
2023-10-23 10:06:26 +0200

    users can no longer be friends multiple times

--- app/Http/Controllers/FriendshipController.php
+++ app/Http/Controllers/FriendshipController.php
@@ -15,6 +15,21 @@ class FriendshipController extends Controller
         $currentUser = Auth::user();
         $userToAdd   = User::where('username', $username)->first();
 
+        if(!$userToAdd) {
+            return back()->withErrors([
+                'error' => 'User not found',
+            ]);
+        }
+
+        foreach ($currentUser->friendships as $friend){
+            $friend->users()->where('user_id', $userToAdd->id)->exists();
+            if($friend) {
+                return back()->withErrors([
+                   'error' => 'Already Friends'
+                ]);
+            }
+        }
+
         $friendship =  new Friendship;
         $friendship->pending = $currentUser->id;
         $friendship->save();
@@ -22,6 +37,8 @@ class FriendshipController extends Controller
         $currentUser->friendships()->attach($friendship->id);
         $userToAdd->friendships()->attach($friendship->id);
 
-        return back();
+        return back()->with([
+            'succes' => 'Friendrequest sendt',
+        ]);
     }
 }

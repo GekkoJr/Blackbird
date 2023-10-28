1cb23c6423967627201611c1259405632c0ada36
GekkoJr <gekkojr@proton.me>
2023-10-28 13:21:24 +0200

    Freinds can now only be added one time

--- app/Http/Controllers/FriendshipController.php
+++ app/Http/Controllers/FriendshipController.php
@@ -12,31 +12,31 @@ class FriendshipController extends Controller
     //
     public function addFriend(Request $request)
     {
-        if(!User::where('username', $request->username)->exists()) {
+        if (!User::where('username', $request->username)->exists()) {
             return back()->withErrors([
                 'error' => 'User not found'
             ]);
         }
 
         $currentUser = Auth::user();
-        $userToAdd   = User::where('username', $request->username)->first();
+        $userToAdd = User::where('username', $request->username)->first();
 
+        if ($currentUser->id === $userToAdd->id) {
+            return back()->withErrors([
+                'error' => 'Are you really that lonely'
+            ]);
+        }
 
+        foreach ($currentUser->getOtherUsersFromFriends() as $user) {
 
-        foreach ($currentUser->getOtherUsersFromFriends() as $userId) {
-            if($currentUser->id === $userId->id) {
-                return back()->withErrors([
-                    'error' => 'Are you really that lonely'
-                ]);
-            }
-            if($userId->id === $userToAdd->id) {
+            if ($user->id === $userToAdd->id) {
                 return back()->withErrors([
                     'error' => 'you are already friends'
                 ]);
             }
         }
 
-        $friendship =  new Friendship;
+        $friendship = new Friendship;
         $friendship->pending = $currentUser->id;
         $friendship->save();
 
@@ -49,16 +49,18 @@ class FriendshipController extends Controller
         ]);
     }
 
-    public function allFriends() {
-        if(!Auth::check()) {
+    public function allFriends()
+    {
+        if (!Auth::check()) {
             return to_route('login');
         }
 
         return Auth::user()->getFriendshipsAndChannels();
     }
 
-    public function getPending() {
-        if(!Auth::check()) {
+    public function getPending()
+    {
+        if (!Auth::check()) {
             return to_route('login');
         }
 

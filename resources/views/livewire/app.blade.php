4582f7f7b95bebcf87f1b77860ffe4c4b6da13dd
GekkoJr <gekkojr@proton.me>
2023-11-02 13:23:31 +0100

    Removed old livewire components and unused stylesheets

--- resources/views/livewire/app.blade.php
+++
@@ -1,5 +0,0 @@
-<div class="mainAppLayout">
-    {{-- Close your eyes. Count to one. That is how long forever feels. --}}
-    <livewire:app-components.menu></livewire:app-components.menu>
-    <livewire:app-components.main-view></livewire:app-components.main-view>
-</div>

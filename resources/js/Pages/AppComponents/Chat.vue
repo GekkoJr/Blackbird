7b9f53ce262c4fa5352be6685aaf41caf0c2294c
GekkoJr <gekkojr@proton.me>
2023-11-03 15:16:11 +0100

    added a missing comma

--- resources/js/Pages/AppComponents/Chat.vue
+++ resources/js/Pages/AppComponents/Chat.vue
@@ -1,6 +1,6 @@
 <script setup>
 import {useForm} from '@inertiajs/vue3'
-import {onUpdated, onMounted ref,  onBeforeMount} from "vue"
+import {onUpdated, onMounted, ref,  onBeforeMount} from "vue"
 
 const props = defineProps({
     channel: String,
